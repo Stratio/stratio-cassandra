b0e7cca6c5805f8efbf561209a16306e9e7c0f56
Andrés de la Peña <adelapena@icaro.local>
2014-11-15 14:30:46 +0100

    Fix sorting.

--- src/java/com/stratio/cassandra/index/LuceneIndex.java
+++ src/java/com/stratio/cassandra/index/LuceneIndex.java
@@ -260,26 +260,27 @@ public class LuceneIndex
     private TopDocs topDocs(IndexSearcher searcher, Query query, Sort sort, ScoreDoc after, int count)
             throws IOException
     {
-        // Use default sort if the query doesn't use relevance
-        if (sort == null)
-        {
-            if (query instanceof ConstantScoreQuery)
-            {
-                FieldDoc start = after == null ? null : (FieldDoc) after;
-                TopFieldCollector tfc = TopFieldCollector.create(this.sort, count, start, true, false, false, false);
-                Collector collector = new EarlyTerminatingSortingCollector(tfc, this.sort, count);
-                searcher.search(query, collector);
-                return tfc.topDocs();
-            }
-            else
-            {
-                return searcher.searchAfter(after, query, count);
-            }
-        }
-        else
-        {
+        if (sort == null) sort = this.sort;
+//        // Use default sort if the query doesn't use relevance
+//        if (sort == null)
+//        {
+//            if (query instanceof ConstantScoreQuery)
+//            {
+//                FieldDoc start = after == null ? null : (FieldDoc) after;
+//                TopFieldCollector tfc = TopFieldCollector.create(this.sort, count, start, true, false, false, false);
+//                Collector collector = new EarlyTerminatingSortingCollector(tfc, this.sort, count);
+//                searcher.search(query, collector);
+//                return tfc.topDocs();
+//            }
+//            else
+//            {
+//                return searcher.searchAfter(after, query, count);
+//            }
+//        }
+//        else
+//        {
             return searcher.searchAfter(after, query, count, sort);
-        }
+//        }
     }
 
     /**
